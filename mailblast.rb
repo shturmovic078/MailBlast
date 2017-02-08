2f8d6df2de3a2f7162b5e531c89c023b4e791c10
shturmovic078 <agolubev1@usnycpww4255.global.publicisgroupe.net>
2017-02-08 14:19:12 -0500

    updated

---
+++ mailblast.rb
@@ -0,0 +1,50 @@
+#Albert Golubev
+#This code will blast the email to yahoo outlook,gmail.
+require "net/smtp"
+require "mail"
+require "gmail"
+#=> true
+
+
+
+puts "I'm about to send email"
+
+
+
+
+
+
+
+options = { :address              => "smtp.gmail.com",
+            :port                 => 587,
+            :domain               => 'gmail.com',
+            :user_name            => 'publicisteam@gmail.com',
+            :password             => '1675team',
+            :authentication       => 'login',
+            :enable_starttls_auto => true
+
+          }
+
+
+
+
+Mail.defaults do
+  delivery_method :smtp, options
+end
+
+
+Mail.deliver do
+  from 'publicisteam@gmail.com'
+       to 'publicisqa1@yahoo.com,publicisteam@gmail.com,albertgolubev@gmail.com,publicisqa@outlook.com'
+      
+
+       
+  subject 'ECRM-7 Email Test'
+  content_type 'text/html; charset=UTF-8'
+  body   File.read('template.txt')
+end
+
+
+#Twitter.update("I'm tweeting with @gem!")
+
+#=> "<meta xmlns=\"http://www.w3.org/1999/xhtml\" content=\"text/html; charset=UTF-8\" http-equiv=\"content-type\" />"
